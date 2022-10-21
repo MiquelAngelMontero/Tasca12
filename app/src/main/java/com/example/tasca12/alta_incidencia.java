d7a18f249f6ee31ed18f19d8f82b17659ff96c18
MiquelAngelMontero <mmontero2@institutjaumehuguet.cat>
2022-10-21 16:47:08 +0200

    2110202216:46

---
+++ app/src/main/java/com/example/tasca12/alta_incidencia.java
@@ -0,0 +1,14 @@
+package com.example.tasca12;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class alta_incidencia extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_alta_incidencia);
+    }
+}
